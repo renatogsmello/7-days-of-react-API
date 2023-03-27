673b0bb945ce091bf96796a54115016d3c46c86e
renato.mello <renato.mello@globalhitss.com.br>
2023-03-27 17:29:41 -0300

    mudanças

--- api/index.js
+++ api/index.js
@@ -10,7 +10,9 @@ sgMail.setApiKey(apiKey)
 
 let products
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
+	res.setHeader("Content-Type", "text/html")
+	res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate")
 	res.end("welcome to Plants API an Sendgrid")
 })
 
